93d87159760584d884a7d6c41692a51b87eeccc1
Patrick <patrick.lopdrup@gmail.com>
2022-10-29 12:26:38 +0200

    Add try catch around client loop

--- src/main/java/client/PrintClient.java
+++ src/main/java/client/PrintClient.java
@@ -13,69 +13,81 @@ public class PrintClient {
         IPrintServer printServer = new PrintServer();
 
         Scanner _scanner = new Scanner(System.in);
+        boolean _isPrintServerStarted = false;
         boolean shouldExit = false;
         while (true) {
             printOptions();
             System.out.print("> ");
             String _input = _scanner.nextLine();
 
-            if (!isInputLegal(printServer, _input)) {
-                System.out.println("Input not legal. Try again.");
-                continue;
-            }
-
             String[] _inputSplit = _input.toLowerCase().split(" ");
             String _type = _inputSplit[0];
             String[] _arg = Arrays.copyOfRange(_inputSplit, 1, _inputSplit.length);
-            switch (_type) {
-                case "exit":
-                    shouldExit = true;
-                    System.out.println("Exiting program...");
-                    break;
-                case "s":
-                    printServer = printServer.start();
-                    break;
-                case "q":
-                    printServer.stop();
-                    break;
-                case "r":
-                    printServer.restart();
-                    break;
-                case "p":
-                    printServer.print(_arg[0], _arg[1]);
-                    break;
-                case "pq":
-                    printServer.queue(_arg[0]);
-                    break;
-                case "PT":
-                    printServer.topQueue(_arg[0], Integer.parseInt(_arg[1]));
-                    break;
-                case "ps":
-                    printServer.status(_arg[0]);
-                    break;
-                case "cr":
-                    printServer.readConfig(_arg[0]);
-                    break;
-                case "cs":
-                    printServer.setConfig(_arg[0], _arg[1]);
-                    break;
-            }
 
-            if (shouldExit)
-                break;
+            if (!isInputLegal(_isPrintServerStarted, _input)) {
+                System.out.println("Input not legal. Try again.");
+                continue;
+            }
+            try {
+                switch (_type) {
+                    case "exit":
+                        shouldExit = true;
+                        System.out.println("Exiting program...");
+                        break;
+                    case "s":
+                        printServer = printServer.start();
+                        _isPrintServerStarted = true;
+                        break;
+                    case "q":
+                        printServer.stop();
+                        _isPrintServerStarted = false;
+                        break;
+                    case "r":
+                        printServer.restart();
+                        break;
+                    case "p":
+                        printServer.print(_arg[0], _arg[1]);
+                        break;
+                    case "pq":
+                        printServer.queue(_arg[0]);
+                        break;
+                    case "PT":
+                        printServer.topQueue(_arg[0], Integer.parseInt(_arg[1]));
+                        break;
+                    case "ps":
+                        printServer.status(_arg[0]);
+                        break;
+                    case "cr":
+                        printServer.readConfig(_arg[0]);
+                        break;
+                    case "cs":
+                        printServer.setConfig(_arg[0], _arg[1]);
+                        break;
+                }
+
+                if (shouldExit)
+                    break;
+            } catch (Exception e) {
+                System.out.println("Error. Try again.");
+                continue;
+            }
         }
     }
 
-    private static boolean isInputLegal(IPrintServer printServer, String input) {
+    private static boolean isInputLegal(boolean isPrintServerStarted, String input) {
         String[] _inputSplit = input.split(" ");
-        if (printServer == null && !_inputSplit[0].equals("start"))
+        if (_inputSplit[0].equals("exit"))
+            return true;
+        if (!_inputSplit[0].equals("s") && !isPrintServerStarted) {
+            System.out.println("Print server is not started!");
             return false;
+        }
         return true;
     }
 
     private static void printOptions() {
         System.out.println(
-                "\nOptions:\n" +
+                "\nOptions: EXIT to exit program.\n" +
 
                 "> Print Service:\n" +
                 "\tS: Start\tQ: Stop\t\tR: Restart\n" +
